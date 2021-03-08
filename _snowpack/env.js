8528de0c7b1268be6e12ca5e063328a75caede91
carlosrivera <carlosrivera@users.noreply.github.com>
2021-03-08 13:25:42 +0000

    deploy: c1c0df7eba4a2a188e8ad0372fc21c615cf22ca1

--- _snowpack/env.js
+++
@@ -1,5 +0,0 @@
-export const SNOWPACK_PUBLIC_TITLE = "CURL from HAR file.";
-export const SNOWPACK_PUBLIC_DESCRIPTION = "Deprecated script to create a CURL call based on a HAR file.";
-export const MODE = "production";
-export const NODE_ENV = "production";
-export const SSR = false;
